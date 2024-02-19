8e15a2bab39705a5e9b6b797b0201f012d1d4fe8
Carlos9934 <128321187+Carlos9934@users.noreply.github.com>
2024-02-18 19:42:22 -0500

    Add files via upload

---
+++ Fraccionario.java
@@ -0,0 +1,30 @@
+public class Fraccionario {
+    private int numerador;
+    private int denominador;
+    
+}
+
+public class Reloj {
+    private int Hora;
+    private int Minutos;
+    private int Segundos;
+
+}
+
+public class fecha {
+    private int Año;
+    private int Mes;
+    private int Día;
+}
+
+public class Proyectil {
+    private int Velocidad;
+    private int Angulo;
+}
+
+public class Tienda{
+    private String nombre;
+    private String
+
+
+}
